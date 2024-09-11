8923a46dd2726a946beac596b82877e72c973d7c
hugo3h <devhugoherrera@gmail.com>
2024-09-11 12:06:17 -0300

    pacote inquirer adicionado

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const start = () => {
     let count = 0
+    // Laço de repetição para o menu do programa
+    // O laço só termina quando o usuário desejar sair
 
-    while(count < 10){
-        console.log(count, "entrou")
-        count++
+    while(true){
+        let opcao = "sair"
+        switch(opcao){
+            case "cadastrar":
+                console.log ("vamos cadastrar")
+                break
+            case "listar":
+                console.log("vamos listar")
+                break
+
+            case "sair":
+                console.log("saindo")
+                return
+        }
     }
 }
 
