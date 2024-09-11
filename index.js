8cca07f96c9148addd389a8d740e3efb50064e42
hugo3h <devhugoherrera@gmail.com>
2024-09-11 12:54:04 -0300

    menu do programa finalizado

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
-const start = () => {
-    let count = 0
+// Extração de códigos do pacote inquirer
+const { select } = require('@inquirer/prompts')
+
+// Função de inicio do programa
+const start = async() => {
+
     // Laço de repetição para o menu do programa
     // O laço só termina quando o usuário desejar sair
 
     while(true){
-        let opcao = "sair"
+
+        // O código usa a função "select" do pacote importado para mostrar uma lista com opções
+        // O código aguarda (await) a seleção e retorna a opção escolhida
+
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
         switch(opcao){
             case "cadastrar":
-                console.log ("vamos cadastrar")
-                break
-            case "listar":
-                console.log("vamos listar")
+                console.log ("Cadastrando meta")
                 break
 
+            case "listar":
+                console.log("listando")
             case "sair":
-                console.log("saindo")
+                console.log ("Saindo...")
+                console.log("See you, space cowboy")
                 return
         }
     }
