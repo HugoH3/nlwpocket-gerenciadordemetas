f24cd562188d0a089900a7ee65e0d60de424bcbc
hugo3h <devhugoherrera@gmail.com>
2024-09-11 14:00:30 -0300

    função de listagem de metas finalizada

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 // Extração de códigos do pacote inquirer
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
+// Declaração das variáveis globais
 let meta = {
     value: "Ler 1 livro",
     checked: false
 }
 let metas = [ meta ]
+
 // Declaração das funções do código
+
+const listarMetas = async() => {
+    const respostas = await checkbox({
+        message: "Navegue até a meta escolhida e use espaço para marcar ou desmarcar como concluida. Pressione enter para salvar suas alterações",
+        choices: [...metas],
+        instructions: false, // Removendo instruções padrão do inquirer
+    })
+
+    // Atribui o valor false para TODAS as metas
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    if (respostas.length == 0){
+        console.log("Nenhuma meta foi selecionada!")
+        return
+    }
+
+    // Checa cada meta até encontrar as que estão incluidas no array das respostas
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+        // Se a meta for a mesma escolhida pelo usuário, altera o valor para true
+        meta.checked = true
+    })
+
+    console.log("Alterações Salvas!")
+}
 const cadastrarMeta = async() => {
 
     // O código utiliza a função input e espera que o usuário insira uma meta
@@ -28,10 +59,8 @@ const cadastrarMeta = async() => {
 const start = async() => {
 
     // Laço de repetição para o menu do programa
-    // O laço só termina quando o usuário desejar sair
 
     while(true){
-
         // O código usa a função "select" do pacote importado para mostrar uma lista com opções
         // O código aguarda (await) a seleção e retorna a opção escolhida
 
@@ -58,11 +87,14 @@ const start = async() => {
 
                 // Espera até que a função async "cadastrarMeta" seja executada por completo
                 await cadastrarMeta()
-                console.log(meta)
+                console.log(metas)
                 break
 
             case "listar":
-                console.log("listando")
+                // Espera até que a função async "listarMetas" seja executada por completo
+                await listarMetas()
+                break
+
             case "sair":
                 console.log ("Saindo...")
                 console.log("See you, space cowboy")
